bf41415d0decf8c2f6eaaf95984e05d81970e82e
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-07 12:16:40 -0500

    modifying a bit App

--- my-favorite-book-summaries/src/App.js
+++ my-favorite-book-summaries/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from "./components/NavBar";
+import Books from "./components/Books";
+import data from "./assets/data";
 
 function App() {
+  const books = Books.map(data=>{})
   return (
-   <div>
-    hello world
+   <div className='main--container'>
+    <NavBar />
+    <div>
+      {books}
+    </div>
    </div>
   );
 }
